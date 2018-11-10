00cddeb83b4159002548e40dc312ea6eafa56166
root <root@classvm219.cs.rutgers.edu>
2018-11-09 22:37:07 -0500

    Finished the code

--- Testing/userapp.c
+++ Testing/userapp.c
@@ -54,7 +54,36 @@ int ioctl_key(int file_desc, int id, char buff[KEY_SIZE]){
 	return ret_val;
 }
 
+int ioctl_encrypt(int file_desc, int id, char buff[MESSAGE_SIZE]) {
+		
+	struct idBuff param;
+	param.id = id;
+	strcpy(param.buff, buff);
+	int ret_val;
+	ret_val = ioctl(file_desc, IOCTL_ENCRYPT, &param);
+	if (ret_val != 0) {
+		printf("failed to print\n");
+	}
+	printf("value is now encrypted\n");
+	printf("%s\n", param.buff);
+	return ret_val;
+}
 
+int ioctl_decrypt(int file_desc, int id, char buff[MESSAGE_SIZE]) {
+	
+	struct idBuff param;
+	param.id = id;	
+	strcpy(param.buff, buff);
+	int ret_val;
+	ret_val = ioctl(file_desc, IOCTL_DECRYPT, &param);
+	if (ret_val != 0) {
+		printf("failed to print\n");
+		
+	}
+	printf("value is now decrypted\n");
+	printf("decrypted value: %s\n", param.buff);
+	return ret_val;
+}
 
 int main(){
         int i, fd, fdtemp;
@@ -91,42 +120,41 @@ int main(){
 				close(fd);
 				return 0;
                 	case 'c':
+				printf("Creating \n");
         	                ioctl_create(fd);
 	                        break;
         	        case 'd':
-				printf("Enter the ID number of the pair you want to destroy: \n");
+				printf("Destorying: \nEnter the ID number of the pair you want to destroy: \n");
 				scanf("%d", &i);
 				ioctl_destroy(fd, i);
 				break;
 	                case 'k':
- 				printf("Enter the ID number of the key you want to set or change: \n");
+ 				printf("Changing key. \n Enter the ID number of the key you want to set or change: \n");
 				scanf("%d", &i);
-				printf("enter key (Max 100 characters): ");
+				printf("enter key (Max 100 characters, ALL CAPS): ");
                 	        scanf(" %[^\n]", key_buf);
 				ioctl_key(fd,i, key_buf);
                        		
-				printf("The key value you've stores is: \n");
-				read(fd, key_buf, sizeof(key_buf));
-				printf("%s \n", key_buf);
 				//write(fd, write_buf, sizeof(write_buf));
                         	
 				break;
 	                case 'n':
- 				printf("Enter the ID number: \n");
+ 				printf("Encrypting: \nEnter the ID number: \n");
 				scanf("%d", &i);
-				printf("enter message (max 1000 characters):\n ");
+				printf("enter message (max 1000 characters, ALL CAPS):\n ");
                 	        scanf(" %[^\n]", write_buf);
 			        // ioctl_encrypt(fd, i, write_buf);
-				
-				fdtemp = open(ENCRYPT_DEVICE "%d",i, O_RDWR);
+				ioctl_encrypt(fd, i, write_buf);
+			/*	fdtemp = open(ENCRYPT_DEVICE "%d",i, O_RDWR);
 				read(fdtemp,read_buf, sizeof(read_buf));
 
                 	        printf("cryptEncrypt%d: %s\n", i, read_buf);
-                        	close(fdtemp);
+                        	close(fdtemp); */
 				break;
 			case 'm':
-				printf("Enter the ID number of the key you want to set or destroy: \n");
+				printf("Decrypting: \nEnter the ID number: \n");
 				scanf("%d", &i);
+				ioctl_decrypt(fd, i, read_buf);
 				//ioctl_decrypt(fd, i, read_buf);
 				break;
 		        default:
